fada39abbb4bad2a6b4e688f0bfddc62c0a95134
Ahmed Mahfooz Ali Khan <ahmedmahfoozalikhan@Ahmeds-MacBook-Air.local>
2024-09-14 19:20:52 +0200

    Frontend Donegit add .

---
+++ Frontend/src/components/Freebook.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+import axios from "axios";
+
+import Cards from "./Cards";
+
+function Freebook({item}) {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log("Response:", res);
+        console.log("Data:", res.data);
+        if (Array.isArray(res.data)) {
+          const data = res.data.filter((data) => data.category === "Free");
+          console.log("Filtered data:", data);
+          setBook(data);
+        } else {
+          console.error("Error: res.data is not an array");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+    getBook();
+  }, []);
+
+  var settings = {
+    dots: true,  
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div>
+          <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Accusantium veritatis alias pariatur ad dolor repudiandae eligendi
+            corporis nulla non suscipit, iure neque earum?
+          </p>
+        </div>
+
+        <div>
+          <Slider {...settings}>
+            {book.map((item) => (
+              <Cards item={item} key={item.id} />
+            ))}
+            
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Freebook;
